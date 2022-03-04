ad1e65a0c68fb21126c048d5835dba1a641ddda1
Martin Larralde <martin.larralde@embl.de>
2022-03-04 18:35:14 +0100

    Add the `tokenize_all` method to `OboLexer`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate pest_derive;
 extern crate pest;
 
 use pest::error::Error;
+use pest::error::ErrorVariant;
 use pest::iterators::Pairs;
 
 /// The OBO format version 1.4 lexer.
@@ -21,5 +22,24 @@ impl OboLexer {
     /// [`pest::Parser::parse`]: https://docs.rs/pest/latest/pest/trait.Parser.html
     pub fn tokenize(rule: Rule, input: &str) -> Result<Pairs<Rule>, Error<Rule>> {
         <Self as pest::Parser<Rule>>::parse(rule, input)
+    }
+
+    /// Tokenize the entirety of an input string.
+    ///
+    /// Contrary to [`OboLexer::tokenize`], this method will return an error if
+    /// if the input is not parsed in its entirety.
+    pub fn tokenize_all(rule: Rule, input: &str) -> Result<Pairs<Rule>, Error<Rule>> {
+        match Self::tokenize(rule, input) {
+            Ok(pairs) if pairs.as_str().len() == input.len() => Ok(pairs),
+            Err(err) => Err(err),
+            Ok(_) => {
+                let variant = ErrorVariant::ParsingError {
+                    positives: vec![rule],
+                    negatives: vec![],
+                };
+                let span = pest::Span::new(input, 0, input.len()).unwrap();
+                Err(Error::new_from_span(variant, span))
+            }
+        }
     }
 }
